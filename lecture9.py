d6cc6fce4a3ff35e1aedd529167de4e2e6e18950
EC2 Default User <ec2-user@ip-172-31-54-218.ec2.internal>
2021-03-16 18:28:18 +0000

    lecture9

---
+++ lecture9.py
@@ -0,0 +1,34 @@
+'''
+lec 9 class
+'''
+
+class car:
+    
+    maker = 'toyota' #shared by all instances
+    
+    def __init__(self,input_model):
+        self.model = input_model #assigined to each instance
+        
+    def report(self):
+        return self.model, self.maker
+        
+#my_corolla = car('corolla')
+
+#my_highlander = car('highlander')
+
+#print(my_corolla.model)
+
+#my_corolla.maker = 'ford'
+#print(my_corolla.report())
+
+#print(my_corolla.maker)
+#print(my_highlander.maker)
+
+#print(my_corolla.model)
+#print(my_highlander.model)
+
+#import lecture8 
+
+#print(lecture8.cal_f(5))
+
+
